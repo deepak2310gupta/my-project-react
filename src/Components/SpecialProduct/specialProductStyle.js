78fcfe141b835eb0a6416bfaa3d4018f5d8b60d3
Deepak Gupta <deepak.gupta1@unthinkable.co>
2023-02-26 16:20:24 +0530

    Main Content UI Completed

---
+++ src/Components/SpecialProduct/specialProductStyle.js
@@ -0,0 +1,8 @@
+export const specialProductStyle = {
+  specialProductCard: {
+    padding: "20px 10px",
+    borderRadius: "10px",
+    backgroundColor: "white",
+  },
+  specialProductContent: {},
+};
--- src/Pages/homeStyle.js
+++ src/Pages/homeStyle.js
@@ -8,4 +8,70 @@ export const homeStyle = {
     fontWeight: "500",
     marginBottom: "30px",
   },
+
+  leftImageWrapper: {
+    position: "relative",
+  },
+  leftImageTextWrapper: {
+    position: "absolute",
+    top: "15%",
+    left: "5%",
+  },
+  mainBannerLeftHeader: {
+    fontSize: "16px",
+    fontWeight: "400",
+    lineHeight: "24px",
+    color: "#bf4800",
+    margin: "0 0 12px",
+    letterSpacing: "0.3px",
+    textTransform: "uppercase",
+  },
+
+  mainBannerLeftSubHeader: {
+    fontSize: "42px",
+    lineHeight: "54px",
+    letterSpacing: "-2px",
+    fontWeight: "500",
+    textTransform: "none",
+  },
+  mainBannerLeftSubDesc: {
+    fontSize: "18px",
+    lineHeight: "28px",
+    letterSpacing: "0.4",
+    color: "#131921",
+  },
+
+  rightImageWrapper: {
+    position: "relative",
+    width:"49%",
+  },
+
+  rightImageTextWrapper: {
+    top: "25%",
+    left: "10%",
+    position:"absolute",
+  },
+  smallBannerRightHeader: {
+    fontSize: "14px",
+    fontWeight: "400",
+    lineHeight: "24px",
+    color:"#bf4800",
+    margin: "0 0 12px",
+    letterSpacing: "0.3px",
+    textTransform: "uppercase",
+  },
+  smallBannerRightSubHeader: {
+    fontSize: "22px",
+    lineHeight: "34px",
+    letterSpacing: "-2px",
+    fontWeight: "500",
+    textTransform: "none",
+  },
+  smallBannerContentDesc:{
+    fontSize: "14px",
+    lineHeight: "22px",
+    letterSpacing: "0.4",
+    color: "#131921",
+  },
+
 };
